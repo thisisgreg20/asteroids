a876f4af70fe836474e562a0666b688179c9a848
Greg Johnson <thisisgreg2.0@gmail.com>
2026-04-27 21:25:59 -0400

    added forward/backward movement

--- player.py
+++ player.py
@@ -1,5 +1,5 @@
 from circleshape import *
-from constants import PLAYER_RADIUS,LINE_WIDTH,PLAYER_TURN_SPEED
+from constants import PLAYER_RADIUS,LINE_WIDTH,PLAYER_TURN_SPEED, PLAYER_SPEED
 
 class Player(CircleShape):
     def __init__(self, x, y):
@@ -24,10 +24,20 @@ class Player(CircleShape):
     def update(self, dt):
         keys = pygame.key.get_pressed()
 
+        if keys[pygame.K_w]:
+            self.move(dt)
         if keys[pygame.K_a]:
             self.rotate(dt * -1)
+        if keys[pygame.K_s]:
+            self.move(dt * -1)
         if keys[pygame.K_d]:
             self.rotate(dt)
 
     def rotate(self, dt):
         self.rotation += PLAYER_TURN_SPEED * dt
+
+    def move(self, dt):
+        unit_vector = pygame.Vector2(0, 1)
+        rotated_vector = unit_vector.rotate(self.rotation)
+        rotated_with_speed_vector = rotated_vector * PLAYER_SPEED * dt
+        self.position += rotated_with_speed_vector
